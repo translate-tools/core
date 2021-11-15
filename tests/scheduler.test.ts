7569cfd4c9431a451f5fdd792406770f3c65ccbe
vitonsky <86191922+vitonsky@users.noreply.github.com>
2021-11-15 13:43:00 +0300

    test: add correct test for Scheduler

--- tests/scheduler.test.ts
+++ tests/scheduler.test.ts
@@ -1,13 +1,15 @@
 import { Scheduler } from '../src/util/Scheduler';
 import { FakeTranslator } from '../src/translators/FakeTranslator';
 
-const text = 'Hello world';
 const from = 'en';
 const to = 'de';
 
-const expectation = `*[${from}-${to}]${text}`;
+const constructExpectation = (text: string) => `*[${from}-${to}]${text}`;
 
 test('test `FakeTranslator`', (done) => {
+	const text = 'Hello world';
+	const expectation = constructExpectation(text);
+
 	const translator = new FakeTranslator();
 	translator
 		.translate(text, from, to)
@@ -23,11 +25,19 @@ test('test `FakeTranslator`', (done) => {
 test('test `Scheduler`', (done) => {
 	const translator = new FakeTranslator();
 	const scheduler = new Scheduler(translator);
-	scheduler
-		.translate(text, from, to)
-		.then((translation) => {
-			expect(typeof translation).toBe('string');
-			expect(translation).toBe(expectation);
+
+	const text = 'Hello world';
+	const textsList = Array(100)
+		.fill(text)
+		.map((val, idx) => `${val} #${idx}`);
+	Promise.all(textsList.map((text) => scheduler.translate(text, from, to)))
+		.then((translations) => {
+			translations.forEach((translation, idx) => {
+				expect(typeof translation).toBe('string');
+
+				const expectation = constructExpectation(`${text} #${idx}`);
+				expect(translation).toBe(expectation);
+			});
 
 			done();
 		})
