f5627675f767bb65d9dc3993afa3081a2cef99cb
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-01-15 02:07:50 +0300

    test(Scheduler): add priority test

--- tests/scheduler.test.ts
+++ tests/scheduler.test.ts
@@ -22,7 +22,6 @@ test('test `FakeTranslator`', (done) => {
 		.catch(done);
 });
 
-// TODO: add test for queue priority
 test('test `Scheduler`', (done) => {
 	const translator = new FakeTranslator();
 	const scheduler = new Scheduler(translator);
@@ -43,4 +42,65 @@ test('test `Scheduler`', (done) => {
 			done();
 		})
 		.catch(done);
+});
+
+test('test scheduler queue priority', (done) => {
+	const translator = new FakeTranslator();
+	const scheduler = new Scheduler(translator, {
+		// Add to stable results
+		taskBatchHandleDelay: 10,
+	});
+
+	// Data set
+	const tasksList = [
+		{
+			text: 'Priority -1',
+			priority: -1,
+		},
+		{
+			text: 'Priority 1',
+			priority: 1,
+		},
+		{
+			text: 'Priority 2',
+			priority: 2,
+		},
+		{
+			text: 'Priority 3',
+			priority: 3,
+		},
+		{
+			text: 'Most low priority',
+			priority: -999,
+		},
+		{
+			text: 'Most high priority',
+			priority: 999,
+		},
+	];
+
+	(async () => {
+		const expectedResults = await Promise.all(
+			tasksList
+				.slice()
+				.sort((a, b) => b.priority - a.priority)
+				.map(({ text }) => translator.translate(text, from, to)),
+		);
+
+		const results: string[] = [];
+
+		await Promise.all(
+			tasksList.map(({ text, priority }) =>
+				scheduler
+					.translate(text, from, to, { priority })
+					.then((translatedText) => results.push(translatedText)),
+			),
+		).catch(done);
+
+		expectedResults.forEach((expectedText, idx) => {
+			expect(results[idx]).toBe(expectedText);
+		});
+	})()
+		.catch(done)
+		.finally(done);
 });
