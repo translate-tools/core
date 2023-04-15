7d489cd1efd3afa1fe6fa3832f8b564b05bffae2
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-04-16 01:57:56 +0300

    test(#44): add tests for util

--- src/util/languages.test.ts
+++ src/util/languages.test.ts
@@ -1,23 +1,44 @@
-import { isLanguageCodeISO639v2, langCodes } from './languages';
+import { getLanguageCodesISO639v2, isLanguageCodeISO639v2, langCodes } from './languages';
 
-test('langCodes are valid langs', () => {
-	langCodes.every((lang) => {
-		const resutl = isLanguageCodeISO639v2(lang);
-		expect(resutl).toBe(true);
+describe('isLanguageCodeISO639v2', () => {
+	test('langCodes are valid langs', () => {
+		langCodes.every((lang) => {
+			const resutl = isLanguageCodeISO639v2(lang);
+			expect(resutl).toBe(true);
+		});
+	});
+
+	test('lang codes are case sensitive', () => {
+		langCodes.every((lang) => {
+			const upperCaseLang = lang.toUpperCase();
+			const resutl = isLanguageCodeISO639v2(upperCaseLang);
+			expect(resutl).toBe(false);
+		});
+	});
+
+	test('random values are not valid langs', () => {
+		['00', 'unknown', 'en-JA'].every((lang) => {
+			const resutl = isLanguageCodeISO639v2(lang);
+			expect(resutl).toBe(false);
+		});
 	});
 });
 
-test('lang codes are case sensitive', () => {
-	langCodes.every((lang) => {
-		const upperCaseLang = lang.toUpperCase();
-		const resutl = isLanguageCodeISO639v2(upperCaseLang);
-		expect(resutl).toBe(false);
+describe('getLanguageCodesISO639v2', () => {
+	test('invalid values are not picked', () => {
+		const result = getLanguageCodesISO639v2(['00', 'unknown', 'qq-JA']);
+		expect(result).toEqual([]);
 	});
-});
 
-test('random values are not valid langs', () => {
-	['00', 'unknown', 'en-JA'].every((lang) => {
-		const resutl = isLanguageCodeISO639v2(lang);
-		expect(resutl).toBe(false);
+	test('no duplicates', () => {
+		const result = getLanguageCodesISO639v2([
+			'00',
+			'unknown',
+			'qq-JA',
+			'en-GB',
+			'en-US',
+			'ja',
+		]);
+		expect(result).toEqual(['en', 'ja']);
 	});
 });
