1f13627e632c15b34ce738a401179aa16214feef
vitonsky <86191922+vitonsky@users.noreply.github.com>
2023-05-01 16:17:22 +0300

    test(#57): update FakeTranslator test

--- src/translators/__tests__/FakeTranslator.test.ts
+++ src/translators/__tests__/FakeTranslator.test.ts
@@ -1,22 +1,32 @@
 import { FakeTranslator } from '../FakeTranslator';
 
-// Test a test utilities
+describe('FakeTranslator', () => {
+	test('method `translate` for single text', async () => {
+		const from = 'en';
+		const to = 'de';
 
-test('test FakeTranslator', (done) => {
-	const from = 'en';
-	const to = 'de';
+		const text = 'Hello world';
+		const expectation = `*[${from}-${to}]${text}`;
 
-	const text = 'Hello world';
-	const expectation = `*[${from}-${to}]${text}`;
-
-	const translator = new FakeTranslator();
-	translator
-		.translate(text, from, to)
-		.then((translation) => {
-			expect(typeof translation).toBe('string');
+		const translator = new FakeTranslator();
+		await translator.translate(text, from, to).then((translation) => {
 			expect(translation).toBe(expectation);
+		});
+	});
 
-			done();
-		})
-		.catch(done);
+	test('method `translateBatch` for multiple texts', async () => {
+		const from = 'en';
+		const to = 'de';
+
+		const text1 = 'Hello world';
+		const expectation1 = `*[${from}-${to}]${text1}`;
+
+		const text2 = 'My name is Jeff';
+		const expectation2 = `*[${from}-${to}]${text2}`;
+
+		const translator = new FakeTranslator();
+		await translator.translateBatch([text1, text2], from, to).then((translation) => {
+			expect(translation).toEqual([expectation1, expectation2]);
+		});
+	});
 });
