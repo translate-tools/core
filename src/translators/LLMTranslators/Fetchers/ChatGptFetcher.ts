8812c07e518067ba5fd07e353ea322cb343a4e51
Irina Katsyuta <katsyuta@proton.me>
2025-03-29 02:50:43 +0100

    feat: add chatgpt fetcher

---
+++ src/translators/LLMTranslators/Fetchers/ChatGptFetcher.ts
@@ -0,0 +1,54 @@
+import { z } from 'zod';
+import { LLMFetcher } from '../LLMFetcher';
+
+export class ChatGptFetcher implements LLMFetcher {
+	constructor(
+		private readonly apiKey: string,
+		private readonly model = 'gpt-4o-mini',
+		private readonly url = 'https://api.openai.com/v1/chat/completions',
+	) {}
+
+	public getLengthLimit() {
+		return 2000;
+	}
+
+	public getRequestsTimeout() {
+		return 500;
+	}
+
+	public async fetch(prompt: string): Promise<string> {
+		const response = await fetch(this.url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${this.apiKey}`,
+			},
+			body: JSON.stringify({
+				model: this.model,
+				messages: [{ role: 'user', content: prompt }],
+			}),
+		});
+
+		if (!response.ok) {
+			throw new Error(
+				`Request failed with status ${response.status}: ${response.statusText}`,
+			);
+		}
+
+		const data = await response.json();
+
+		// validate response structure
+		const parseResult = z
+			.object({
+				choices: z
+					.array(z.object({ message: z.object({ content: z.string() }) }))
+					.min(1),
+			})
+			.parse(data);
+
+		// a list of chat completion choices, there can be more than one only if specified directly.
+		// sourse: https://platform.openai.com/docs/api-reference/chat/object#chat/object-choices
+
+		return parseResult.choices[0].message.content;
+	}
+}
