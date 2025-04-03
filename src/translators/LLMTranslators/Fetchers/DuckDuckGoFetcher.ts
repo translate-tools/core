830e1780c3e66c25f38c75117102359528e8b54a
Irina Katsyuta <katsyuta@proton.me>
2025-04-03 21:59:06 +0200

    refactor: move method to function, change model

--- src/translators/LLMTranslators/Fetchers/DuckDuckGoFetcher.ts
+++ src/translators/LLMTranslators/Fetchers/DuckDuckGoFetcher.ts
@@ -1,40 +1,45 @@
+import { z } from 'zod';
 import { LLMFetcher } from '../LLMTranslatorTypes';
+
+function processRawText(rawText: string) {
+	const texts: {
+		message: string;
+		created: number;
+	}[] = [];
+
+	for (const line of rawText.split('\n\n')) {
+		const prefix = 'data: ';
+		if (!line.startsWith(prefix)) continue;
+
+		const text = line.slice(prefix.length);
+		if (text === '[DONE]') break;
+
+		const json = JSON.parse(text);
+		if (json.message === undefined) continue;
+
+		const validText = z
+			.object({ message: z.string(), created: z.number() })
+			.parse(json);
+
+		texts.push(validText);
+	}
+
+	return texts
+		.sort((a, b) => a.created - b.created)
+		.map((i) => i.message)
+		.join('');
+}
 
 export class DuckDuckGoFetcher implements LLMFetcher {
 	private key: string | null = null;
 
 	constructor(
-		private readonly model = 'gpt-4o-mini',
+		private readonly model = 'o3-mini',
 		private readonly userAgent = 'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0',
 	) {}
 
 	public getLengthLimit = () => 2300;
 	public getRequestsTimeout = () => 2000;
-
-	private processRawText(rawText: string) {
-		const texts: {
-			message: string;
-			created: number;
-		}[] = [];
-
-		for (const line of rawText.split('\n\n')) {
-			const prefix = 'data: ';
-			if (!line.startsWith(prefix)) continue;
-
-			const text = line.slice(prefix.length);
-			if (text === '[DONE]') break;
-
-			const json = JSON.parse(line.slice(prefix.length));
-			if (json.message === undefined) continue;
-
-			texts.push(json);
-		}
-
-		return texts
-			.sort((a, b) => a.created - b.created)
-			.map((i) => i.message)
-			.join('');
-	}
 
 	public async getKey() {
 		if (!this.key) {
@@ -88,7 +93,7 @@ export class DuckDuckGoFetcher implements LLMFetcher {
 		});
 
 		if (!response.ok) {
-			// when the key is not valid or depricated server send 400 status
+			// when the key is not valid or deprecated server send 400 status
 			// This is not a specific status, but we can try reloading the key
 			// In the next method call, a new key is will be required
 			if (response.status === 400) {
@@ -100,6 +105,6 @@ export class DuckDuckGoFetcher implements LLMFetcher {
 		}
 
 		const responseText = await response.text();
-		return this.processRawText(responseText);
+		return processRawText(responseText);
 	}
 }
