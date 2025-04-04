b92176c4788a329c3399e25360d19a99b07a7149
Irina Katsyuta <katsyuta@proton.me>
2025-04-04 17:32:49 +0200

    refactor: improve code

--- src/translators/LLMTranslators/LLMTranslator.ts
+++ src/translators/LLMTranslators/LLMTranslator.ts
@@ -1,35 +1,64 @@
 import { z } from 'zod';
-import { BaseTranslator } from '../BaseTranslator';
 import { LLMFetcher } from './LLMTranslatorTypes';
-
-export type LLMTranslatorOptions = {
-	getPrompt?: (texts: string[], from: string, to: string) => string;
-	retries?: number;
-};
+import { TranslatorInstanceMembers } from '../Translator';
 
 type LLMTranslatorConfig = {
 	getPrompt: (texts: string[], from: string, to: string) => string;
-	retries: number;
+	/**
+	 * The retryLimit - number of retries on error, must be a natural number
+	 */
+	retryLimit: number;
 };
 
-export class LLMTranslator extends BaseTranslator {
+const getPrompt = (text: string[], from: string, to: string) => {
+	return `You are a text translation service. I will provide an array of texts, and your task is to translate them from language ${from} to language ${to}.
+If I specify the source language as 'auto', you should automatically detect it and translate it into the target language I set.
+Do not add any explanations — translate strictly according to the content. Be careful when creating an array, it must be syntactically correct and do not change quotation marks. Return an array of translated texts while preserving their order.
+Here is the array of texts: ${JSON.stringify(text)}`;
+};
+
+export class LLMTranslator implements TranslatorInstanceMembers {
 	private readonly config: LLMTranslatorConfig;
 
-	constructor(private readonly llm: LLMFetcher, options?: LLMTranslatorOptions) {
-		super();
-
+	constructor(
+		private readonly llm: LLMFetcher,
+		options?: Partial<LLMTranslatorConfig>,
+	) {
 		this.config = {
 			...options,
-			retries: options?.retries ?? 3,
-			getPrompt:
-				options?.getPrompt ??
-				((text: string[], from: string, to: string) => {
-					return `You are a text translation service. I will provide an array of texts, and your task is to translate them from language ${from} to language ${to}.
-If I specify the source language as 'auto', you should automatically detect it and translate it into the target language I set.
-Do not add any explanations—translate strictly according to the content. Be careful when creating an array, it must be syntactically correct and do not change quotation marks. Return an array of translated texts while preserving their order.
-Here is the array of texts: ${JSON.stringify(text)}`;
-				}),
+			retryLimit: options?.retryLimit ?? 3,
+			getPrompt: options?.getPrompt ?? getPrompt,
 		};
+	}
+
+	public async translate(text: string, from: string, to: string): Promise<string> {
+		return this.translateBatch([text], from, to).then((resp) => resp[0]);
+	}
+
+	public async translateBatch(text: string[], from: string, to: string) {
+		let attempt = 0;
+
+		while (true) {
+			try {
+				const response = await this.llm.fetch(
+					this.config.getPrompt(text, from, to),
+				);
+
+				// response length should be equal to request length
+				const validateResult = z
+					.string()
+					.array()
+					.min(text.length)
+					.parse(JSON.parse(response));
+
+				return validateResult;
+			} catch (error) {
+				if (attempt + 1 === this.config.retryLimit) {
+					throw error;
+				}
+				attempt++;
+			}
+		}
 	}
 
 	public getLengthLimit() {
@@ -40,33 +69,9 @@ Here is the array of texts: ${JSON.stringify(text)}`;
 		return this.llm.getRequestsTimeout();
 	}
 
-	public async translate(text: string, from: string, to: string): Promise<string> {
-		return this.translateBatch([text], from, to).then((resp) => resp[0]);
-	}
-
-	public async translateBatch(text: string[], from: string, to: string) {
-		for (let count = 0; count < this.config.retries; count++) {
-			try {
-				const response = await this.llm.fetch(
-					this.config.getPrompt(text, from, to),
-				);
-
-				// validate response
-				const validateResult = z.array(z.string()).parse(JSON.parse(response));
-
-				if (validateResult.length !== text.length) {
-					throw new Error(
-						`Unexpected response: expected an array of length ${text.length}, but received an array of length ${validateResult.length}.`,
-					);
-				}
-
-				return validateResult;
-			} catch (error) {
-				if (count === this.config.retries - 1) {
-					throw error;
-				}
-			}
-		}
-		throw new Error('Failed to translate');
+	public checkLimitExceeding(text: string | string[]) {
+		const plainText = Array.isArray(text) ? text.join('') : text;
+		const extra = plainText.length - this.getLengthLimit();
+		return extra > 0 ? extra : 0;
 	}
 }
