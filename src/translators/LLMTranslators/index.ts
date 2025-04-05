9e1c9169eb0d84bab479ed9e689c2dcc81f64493
Irina Katsyuta <katsyuta@proton.me>
2025-04-06 00:45:18 +0200

    fix: duckduckgo slice string, update prompt

--- src/translators/LLMTranslators/index.ts
+++ src/translators/LLMTranslators/index.ts
@@ -28,7 +28,7 @@ export type LLMTranslatorConfig = {
 
 const getPrompt = (text: string[], from: string, to: string) => {
 	return `You are a text translation service. I will provide an array of texts, and your task is to translate them from language ${from} to language ${to}.
-If I specify the source language as 'auto', you should automatically detect it and translate it into the target language I set.
+If I specify the source language as 'auto', you should automatically detect it and translate it into the target language I set. If array has one string, return array of same length.
 Do not add any explanations — translate strictly according to the content. Be careful when creating an array, it must be syntactically correct and do not change quotation marks. Return an array of translated texts while preserving their order.
 Here is the array of texts: ${JSON.stringify(text)}`;
 };
