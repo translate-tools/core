e42cf2a0df657e0407be83dff476e15efa9b7da5
Irina Katsyuta <katsyuta@proton.me>
2025-04-11 02:34:48 +0200

    chore: sort prompt

--- src/translators/LLMTranslators/LLMTranslator.ts
+++ src/translators/LLMTranslators/LLMTranslator.ts
@@ -24,8 +24,8 @@ export type LLMTranslatorConfig = {
 export const getPrompt = (text: string[], from: string, to: string) => {
 	return `You are a text translation service. I will provide an array of texts, and your task is to translate them from language ${from} to language ${to}.
 If I specify the source language as 'auto', you should automatically detect it and translate it into the target language I set.
-If the array contains exactly one text, return a array with one element containing the complete, unified translation (do not split it into multiple strings).
-Do not add any explanations — translate strictly according to the content. Be careful when creating an array; it must be syntactically correct and do not change quotation marks. Return an array of translated texts while preserving their order.
+The array in your response must be the same length as the one in the request. Do not add any explanations — translate strictly according to the content. 
+Be careful when creating an array; it must be syntactically correct and do not change quotation marks. Return an array of translated texts while preserving their order.
 Here is the JSON array of texts: ${JSON.stringify(text)}`;
 };
 
