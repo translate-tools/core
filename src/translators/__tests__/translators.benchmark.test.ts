f334313ddc154f548012947002c584d812f3eba2
Robert Vitonsky <robert@vitonsky.net>
2025-07-16 14:19:02 +0200

    test: add translators benchmark

---
+++ src/translators/__tests__/translators.benchmark.test.ts
@@ -0,0 +1,34 @@
+import { getTranslatorsScore } from './benchmark';
+
+const referenceText = `
+Linguist is a powerful browser extension that is ready to replace your favorite translation service.
+
+Translate web pages, highlighted text, Netflix subtitles, private messages, speak the translated text, and save important translations to your personal dictionary to learn new words in 130 languages.
+
+Why Linguist?
+
+Unlike other browser extensions, Linguist is not just a wrapper over the Google Translator Widget; it's a full-featured and independent translation system. This is why with Linguist you can be private and translate texts offline on your device and use any translation service, even your own like ChatGPT. See a custom translators list to find bindings for the most popular translation services.
+
+Linguist is a free, open-source project, respects your privacy, and does not collect your personal data.
+`.trim();
+
+const referenceTranslation = `
+Linguist - это мощное расширение для браузера, готовое заменить ваш любимый сервис перевода.
+
+Переводите веб-страницы, выделенный текст, субтитры Netflix, приватные сообщения, озвучивайте переведённый текст и сохраняйте важные переводы в словарь, чтобы изучать новые слова на 130 языках.
+
+Почему Linguist?
+
+В отличие от других расширений, Linguist не просто обёртка над Google Translator Widget; это полнофункциональная и независимая система перевода. Поэтому с Linguist вы можете сохранять конфиденциальность, переводя тексты офлайн прямо на своём устройстве и использовать любой сервис перевода, даже собственный, как например ChatGPT. Ознакомьтесь со списком пользовательских переводчиков, чтобы найти адаптеры к самым популярным сервисам.
+
+Linguist бесплатный проект с открытым исходным кодом, уважает вашу конфиденциальность и не собирает личные данные.
+`.trim();
+
+test('Top translators list with score', async () => {
+	await expect(
+		getTranslatorsScore({
+			text: referenceText,
+			translation: referenceTranslation,
+		}).then((results) => results.map(({ name }) => name)),
+	).resolves.matchSnapshot();
+});
