8ef5991d32d0d210311b5af5dac04c47c454b5fd
Robert Vitonsky <robert@vitonsky.net>
2025-07-17 19:51:45 +0200

    test: add test cases for google translator utils

---
+++ src/translators/GoogleTranslator/GoogleTranslator.utils.test.ts
@@ -0,0 +1,34 @@
+import { parseXMLResponse } from '.';
+import { visitArrayItems } from './utils';
+
+describe('parseXMLResponse', () => {
+	test('Single string is returned as is', () => {
+		expect(
+			parseXMLResponse('<pre><a i="0">Demo -Text zur Übersetzung</a></pre>'),
+		).toBe('Demo -Text zur Übersetzung');
+	});
+
+	test('Few elements is joined with space', () => {
+		expect(
+			parseXMLResponse(
+				'<pre><a i="0">single text</a><a i="1">another text</a></pre>',
+			),
+		).toBe('single text another text');
+	});
+});
+
+describe('visitArrayItems', () => {
+	test('Flat array', () => {
+		const callback = vi.fn();
+		visitArrayItems(['foo', 'bar', 'baz', 'qux'], callback);
+
+		expect(callback.mock.calls).toStrictEqual([['foo'], ['bar'], ['baz'], ['qux']]);
+	});
+
+	test('Nested arrays', () => {
+		const callback = vi.fn();
+		visitArrayItems([['foo', 'bar'], 'baz', ['qux']], callback);
+
+		expect(callback.mock.calls).toStrictEqual([['foo'], ['bar'], ['baz'], ['qux']]);
+	});
+});
