c7ac1da7aaddd70b935f3566d2265a56090423d0
Robert Vitonsky <robert@vitonsky.net>
2025-09-09 13:42:06 +0200

    test: remove lingva TTS of tests

--- src/tts/__tests__/tts.test.ts
+++ src/tts/__tests__/tts.test.ts
@@ -3,41 +3,36 @@ import { parseBuffer } from 'music-metadata';
 import { isLanguageCodeISO639v1 } from '../../languages/codes';
 
 import { GoogleTTS } from '../GoogleTTS';
-import { LingvaTTS } from '../LingvaTTS';
 import { TTSProvider } from '../types';
 
-const ttsConstructor: TTSProvider[] = [GoogleTTS, LingvaTTS];
+const ttsConstructor: TTSProvider[] = [GoogleTTS];
 
 ttsConstructor.map((ttsConstructor) => {
 	describe(`methods of TTS class "${ttsConstructor.name}"`, () => {
-		test(
-			`getAudioBuffer returns audio buffer`,
-			async () => {
-				const tts = new ttsConstructor();
-				const audioBuffer = await tts.getAudioBuffer(
-					'Hello world. This is a demo text from TTS module',
-					'en',
-				);
+		test(`getAudioBuffer returns audio buffer`, async () => {
+			const tts = new ttsConstructor();
+			const audioBuffer = await tts.getAudioBuffer(
+				'Hello world. This is a demo text from TTS module',
+				'en',
+			);
 
-				expect(audioBuffer.buffer).toBeInstanceOf(ArrayBuffer);
-				expect(typeof audioBuffer.type).toBe('string');
+			expect(audioBuffer.buffer).toBeInstanceOf(ArrayBuffer);
+			expect(typeof audioBuffer.type).toBe('string');
 
-				// Parse as audio file
-				const parsedAudioFrame = await parseBuffer(
-					new Uint8Array(audioBuffer.buffer),
-				);
+			// Parse as audio file
+			const parsedAudioFrame = await parseBuffer(
+				new Uint8Array(audioBuffer.buffer),
+			);
 
-				expect(parsedAudioFrame.format.bitrate).toBe(64000);
-				expect(parsedAudioFrame.format.codec).toBe('MPEG 2 Layer 3');
-				expect(parsedAudioFrame.format.container).toBe('MPEG');
-				expect(parsedAudioFrame.format.duration).toBeGreaterThan(0);
-				expect(parsedAudioFrame.format.hasAudio).toBe(true);
-				expect(parsedAudioFrame.format.hasVideo).toBe(false);
-				expect(parsedAudioFrame.format.sampleRate).toBe(24000);
-				expect(parsedAudioFrame.format.numberOfSamples).toBeGreaterThan(100_000);
-			},
-			1000 * 15,
-		);
+			expect(parsedAudioFrame.format.bitrate).toBe(64000);
+			expect(parsedAudioFrame.format.codec).toBe('MPEG 2 Layer 3');
+			expect(parsedAudioFrame.format.container).toBe('MPEG');
+			expect(parsedAudioFrame.format.duration).toBeGreaterThan(0);
+			expect(parsedAudioFrame.format.hasAudio).toBe(true);
+			expect(parsedAudioFrame.format.hasVideo).toBe(false);
+			expect(parsedAudioFrame.format.sampleRate).toBe(24000);
+			expect(parsedAudioFrame.format.numberOfSamples).toBeGreaterThan(100_000);
+		});
 
 		// Disable test, to allow TTS to return any lang codes they support
 		// Users must filter lang codes on their side, to ensure valid ISO codes
